9fa2fedc3ff3064b469e980c99cfd7611f3d8609
Kirill <kirilldanilovski@yandex.ru>
2022-07-11 12:42:40 +0300

    Поправил

--- ITMO.CPLUSPLUS.Examine/ITMO.CPLUSPLUS.Examine.cpp
+++ ITMO.CPLUSPLUS.Examine/ITMO.CPLUSPLUS.Examine.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <fstream> 
+#include <tuple>
 using namespace std;
 
 class Human {
@@ -21,31 +22,52 @@ public:
     {
         return name;
     }
-
+    void set_lastname(string person_lastname)
+    {
+        last_name = person_lastname;
+    }
+    // Получение имени
+    string get_lastname()
+    {
+        return last_name;
+    }
+    
 
     Human() {
-        string name;
-        string last_name;
-        int phone;
-        string position;
+
+            string name;
+            string last_name;
+            int phone;
+            string position;
+        
+        
     }
+
+    
 
 
     Human(string name, string last_name, int phone, string position) {
-        while (name != "exit")
-        {
+        
             this->name = name;
             this->last_name = last_name;
             this->phone = phone;
             this->position = position;
-        }
+        
+            
         
     }
-    friend class Menu;
+    /*void ShowAll() {
+        tuple<string, string, int, string> tup1(std::make_tuple(name, last_name, phone, position));
+        std::cout << std::get<0>(tup1) << " " << std::get<1>(tup1) << " " << std::get<2>(tup1) << " " << std::get<3>(tup1) << std::endl;
+        
+    }*/
+    
+    //friend class Menu;
     
     void EnterInfoHuman() {
         
         //cout << "Enter 'exit' to name for stoping " << endl;
+        
         cout << "input name: ";
         cin >> name;
          
@@ -60,26 +82,19 @@ public:
     };
 
     void SaveAllHuman() {
-            
-            
                 ofstream save("Address book.txt", ios::app);
                 save << "name - " << name << endl;
                 save << "last_name - : " << last_name << endl;
                 save << "phone - : " << phone << endl;
                 save << "position - : " << position << endl;
-                
                 save << endl;
                 save.close();
-
-            
-            
-
     }
 
 };
 
-class Menu {
-    friend class Human;
+class Menu : public Human {
+    //friend class Human;
 public:
     void ChooseMenu() {
         int x;
@@ -90,20 +105,21 @@ public:
          
         cin >> x;
         string choose;
-        Human h1;
+        
         switch (x) {
         case 1:
             
-            h1.EnterInfoHuman();
-            h1.SaveAllHuman();
+            EnterInfoHuman();
+            SaveAllHuman();
         case 2:
             
             cout << "Continue ? (yes/no)" << endl;
             cin >> choose;
             while (choose == "yes")
             {
-                h1.EnterInfoHuman();
-                h1.SaveAllHuman();
+                EnterInfoHuman();
+                SaveAllHuman();
+
                 cout << "Continue ? (yes/no)" << endl;
                 cin >> choose;
             }
@@ -113,9 +129,12 @@ public:
     }
 };
 
+
+
 int main()
 {  
     Menu m1;
     m1.ChooseMenu();
+
 }
 
